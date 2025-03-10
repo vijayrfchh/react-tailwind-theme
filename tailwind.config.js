0eaed9223205737c88abbcf6c188d760a333f335
vijayrfchh <Vijay.rfchh@gmail.com>
2025-03-10 15:22:55 +0530

    initial stepup for theme

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],  
+// }
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1D4ED8",
+        secondary: "#9333EA",
+        accent: "#FBBF24",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+
